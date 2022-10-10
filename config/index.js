90b73f3b7b4fcd5d0f7bcfacd9c6a3c84a3b73cb
ctaot <98950452+ctaot@users.noreply.github.com>
2022-10-10 10:12:13 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -99,7 +99,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "orrqs6Q8Ehbrjavp0UPz50F5K0CM",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "45K1s2Jnh2RTiZ5PhIs3yD2EwhQB4UE_bxXQOaNJ868",
+      useTemplateId: "Rvh-uq9cpm_7EyedfsVvpS_udYWmJ0wf7AIZBd8HS9M",
       // 所在省份
       province: "湖南",
       // 所在城市
